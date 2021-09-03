ab096b982166a214ad90f219c57e63c42475771a
sdi18000 <magoxhaj000@hotmail.gr>
2021-09-03 19:59:58 +0300

    Update Moderator.java

--- backend/src/main/java/com/project/linkedin/Moderator/Moderator.java
+++ backend/src/main/java/com/project/linkedin/Moderator/Moderator.java
@@ -2,6 +2,24 @@ package com.project.linkedin.Moderator;
 
 
 
+import lombok.Data;
 
+import javax.persistence.*;
+
+
+@Data
+@Entity
 public class Moderator {
+
+    private @Id @GeneratedValue(strategy = GenerationType.SEQUENCE) Long ididModerator;
+    private Long User_idUser;
+
+    public Moderator() {
+    }
+
+    public Moderator( Long User_idUser) {
+
+        this.User_idUser = User_idUser;
+
+    }
 }
